49da0049b527880c5305d3bd5228893851304850
Yoshinori SATO <sato@baby-calendar.jp>
2019-10-02 17:21:37 +0900

    Super Cache投入

--- config/application.php
+++ config/application.php
@@ -130,6 +130,10 @@ Config::define('AS3CF_SETTINGS', serialize([
   'object-versioning' => true,
 ]));
 
+// WP Super Cache
+define('WP_CACHE', true);
+define('WPCACHEHOME', $webroot_dir . '/app/plugins/wp-super-cache/');
+
 $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
 
 if (file_exists($env_config)) {
